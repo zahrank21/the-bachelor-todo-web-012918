5df9c4bf6f02ed28b9e5ef096b38b6dac965268a
zahrank21 <zahrank@gmail.com>
2018-01-14 17:21:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -60,8 +60,8 @@ def get_average_age_for_season(data, season)
 
 
   total = ages.length
-  
-    
-    (ages.map{|el| el.to_f}.reduce(:+) / total).to_i
+
+
+    (ages.map{|el| el.to_f}.reduce(:+) / total)
 
 end
