0e5b47d6b128a23480a7413e63d44b5c0fdc36ae
zahrank21 <zahrank@gmail.com>
2018-01-14 17:24:23 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,67 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_hash = []
+  data[season].each do |el|
+    if el.values.include?("Winner")
+      winner_hash = el
+    end
+  end
+
+
+  winner_hash["name"].split.first
+
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_hash = []
+
+  data.each do |season, conts|
+    conts.each do |cont_hash|
+
+      if cont_hash.values.include?(occupation)
+        contestant_hash = cont_hash
+      end
+    end
+  end
+  contestant_hash["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, conts|
+    conts.each do |cont_hash|
+
+      if cont_hash.values.include?(hometown)
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, conts|
+    conts.each do |cont_hash|
+
+      if cont_hash.values.include?(hometown)
+        return cont_hash["occupation"]
+      end
+    end
+  end
+
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data[season].each do |el|
+    if el.keys.include?("age")
+      ages << el["age"]
+    end
+  end
+
+
+  total = ages.length
+
+
+  (ages.map{|el| el.to_f}.reduce(:+) / total).round
+
 end
