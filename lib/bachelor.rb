ece93c96f005ef404d314f50893dfbe162880175
zahrank21 <zahrank@gmail.com>
2018-01-14 16:41:08 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -26,7 +26,16 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, conts|
+    conts.each do |cont_hash|
+
+      if cont_hash.values.include?(hometown)
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
